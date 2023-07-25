d764bcdfe41816164c9fdf13fe1231b1f0adbe96
Thierry Bleau <thierry.bleau@protonmail.com>
2023-07-25 17:32:45 -0400

    diff wrap, unwrap, send and other coupled with transfer event

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,7 @@ mod abi;
 mod pb;
 
 
-use abi::wsteth;
-use substreams_ethereum::pb::eth::v2 as eth;
+use substreams_ethereum::{pb::eth::v2 as eth, Event, Function};
 use substreams::{
     log, hex,
     store::{DeltaProto, Deltas, StoreNew, StoreSet, StoreSetProto},
@@ -13,24 +12,31 @@ use std::str::FromStr;
 use substreams::store::{DeltaBigInt, StoreAdd, StoreAddBigInt};
 use substreams::scalar::BigInt;
 
-use pb::eth::wsteth::v1::Transactions;
-use pb::eth::wsteth::v1::Transaction;
+
 use pb::eth::wsteth::v1::Token;
-use pb::eth::wsteth::v1::Account;
+use pb::eth::wsteth::v1::Actions;
+use pb::eth::wsteth::v1::Action;
+
+use pb::eth::wsteth::v1::ActionType;
+use pb::eth::wsteth::v1::Transfers;
+use pb::eth::wsteth::v1::Transfer;
+
 
 use abi::wsteth::functions::Wrap as WrapCall;
 use abi::wsteth::functions::Unwrap as UnwrapCall;
+use abi::wsteth::functions::Transfer as TransferCall;
+
 use abi::wsteth::events::Transfer as WSTETHTransfer;
 
 
 #[substreams::handlers::map]
-pub fn map_transactions(block: eth::Block) -> Result<Transactions, substreams::errors::Error> {
-    Ok(Transactions {
-        transactions: get_transactions(&block).collect(),
+pub fn map_actions(block: eth::Block) -> Result<Actions, substreams::errors::Error> {
+    Ok(Actions {
+        actions: get_actions(&block).collect(),
     })
 }
 
-fn get_transactions<'a>(block: &'a eth::Block) -> impl Iterator<Item = Transaction> + 'a {
+fn get_actions<'a>(block: &'a eth::Block) -> impl Iterator<Item = Action> + 'a {
     block
     .transactions()
     .filter_map(
@@ -41,43 +47,89 @@ fn get_transactions<'a>(block: &'a eth::Block) -> impl Iterator<Item = Transacti
             _ => None
         }
     )
-    .map(
-        |tx| Transaction {
-            from: Hex(&tx.from).to_string(),
-            to: Hex(&tx.to).to_string(),
-            amount: String::from("0"),
-            tx_hash: Hex(&tx.hash).to_string(),
-        }
+    .flat_map(
+        |tx| 
+        tx
+        .logs_with_calls()
+        .map(
+            |(log, call)| {
+
+                if let Some(_) = WrapCall::match_and_decode(call){
+                    (ActionType::Wrap, call, log)
+                } else if let Some(_) = UnwrapCall::match_and_decode(call) {
+                    (ActionType::Unwrap, call, log)
+                } else if let Some(_) = TransferCall::match_and_decode(call){
+                    (ActionType::Send, call, log)
+                } else {
+                    (ActionType::Other, call, log)
+                }
+            }
+        )
+        .map(
+            |(a, c,l)| {
+                let (from, to, value) = if let Some(t) = WSTETHTransfer::match_and_decode(l) {
+                    (Hex(t.from).to_string(), Hex(t.to).to_string(), t.value.to_string())
+                } else {
+                    (String::from(""),String::from(""), String::from(""))
+                };
+                
+                let hash = Hex(&tx.hash).to_string();
+                let b_n = &block.number.to_string();
+                let timestamp = &block.timestamp_seconds().to_string();
+
+                Action {
+                    action_type: a.into(),
+                    method: Hex(&c.call.input).to_string(),
+                    account: Hex(&tx.from).to_string(),
+                    amount: value.to_string(),
+                    transfer: Some(Transfer {
+                        from: from,
+                        to: to,
+                        amount: value.to_string(),
+                        tx_hash: hash.to_string(),
+                        block_number: b_n.to_string(),
+                        timestamp: timestamp.to_string(),
+                        log_index: l.index.to_string()
+                    }),
+                    tx_hash: hash,
+                    block_number: b_n.to_string(),
+                    timestamp: timestamp.to_string()
+
+                }
+            }
+            
+        )
     )
 }
 
-#[substreams::handlers::store]
-pub fn store_token(block: eth::Block, o: StoreSetProto<Token>) {
-    if block.number == 11888477 as u64 {
-        let token = Token {
-            name: "wstETH".to_string(),
-            address: String::from("0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"),
-            decimal: "18".to_string(),
-            symbol: "WSTETH".to_string(),
-        };
-        o.set(0, format!("Token: {}", token.address), &token);
-    };
-}
 
-#[substreams::handlers::store]
-pub fn store_account_holdings(txs: Transactions, o: StoreAddBigInt) {
-    for tx in txs.transactions {
-        o.add(
-            0,
-            format!("Account: {}", &tx.from.to_string()),
-            BigInt::from_str(tx.amount.as_str()).unwrap().neg(),
-        );
+// #[substreams::handlers::store]
+// pub fn store_token(block: eth::Block, o: StoreSetProto<Token>) {
+//     if block.number == 11888477 as u64 {
+//         let token = Token {
+//             name: "wstETH".to_string(),
+//             address: String::from("0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"),
+//             decimal: "18".to_string(),
+//             symbol: "WSTETH".to_string(),
+//         };
+//         o.set(0, format!("Token: {}", token.address), &token);
+//     };
+// }
 
-        o.add(
-            0,
-            format!("Account: {}", &tx.to.to_string()),
-            BigInt::from_str(tx.amount.as_str()).unwrap(),
-        );
-    }
+// #[substreams::handlers::store]
+// pub fn store_account_holdings(txs: Transactions, o: StoreAddBigDecimal) {
+//     for tx in txs.transactions {
+//         o.add(
+//             0,
+//             format!("Account: {}", &tx.from.to_string()),
+//             BigInt::from_str(tx.amount.as_str()).unwrap().neg(),
+//         );
 
-}
+//         o.add(
+//             0,
+//             format!("Account: {}", &tx.to.to_string()),
+//             BigInt::from_str(tx.amount.as_str()).unwrap(),
+//         );
+//     }
+
+// }
--- src/pb/eth.wsteth.v1.rs
+++ src/pb/eth.wsteth.v1.rs
@@ -1,13 +1,7 @@
 // @generated
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Transactions {
-    #[prost(message, repeated, tag="1")]
-    pub transactions: ::prost::alloc::vec::Vec<Transaction>,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Transaction {
+pub struct Transfer {
     #[prost(string, tag="1")]
     pub from: ::prost::alloc::string::String,
     #[prost(string, tag="2")]
@@ -16,36 +10,18 @@ pub struct Transaction {
     pub amount: ::prost::alloc::string::String,
     #[prost(string, tag="4")]
     pub tx_hash: ::prost::alloc::string::String,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Transfer {
-    #[prost(message, optional, tag="1")]
-    pub from: ::core::option::Option<Account>,
-    #[prost(message, optional, tag="2")]
-    pub to: ::core::option::Option<Account>,
-    #[prost(string, tag="3")]
-    pub amount: ::prost::alloc::string::String,
-    #[prost(string, tag="4")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(uint64, tag="5")]
-    pub block_number: u64,
-    #[prost(uint64, tag="6")]
-    pub timestamp: u64,
-    #[prost(uint32, tag="7")]
-    pub log_index: u32,
+    #[prost(string, tag="5")]
+    pub block_number: ::prost::alloc::string::String,
+    #[prost(string, tag="6")]
+    pub timestamp: ::prost::alloc::string::String,
+    #[prost(string, tag="7")]
+    pub log_index: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Transfers {
     #[prost(message, repeated, tag="1")]
     pub transfers: ::prost::alloc::vec::Vec<Transfer>,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Account {
-    #[prost(string, tag="1")]
-    pub address: ::prost::alloc::string::String,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -64,14 +40,14 @@ pub struct Token {
 pub struct Action {
     #[prost(enumeration="ActionType", tag="1")]
     pub action_type: i32,
-    #[prost(message, optional, tag="2")]
-    pub account: ::core::option::Option<Account>,
-    #[prost(string, tag="3")]
-    pub steth_amount: ::prost::alloc::string::String,
+    #[prost(string, tag="9")]
+    pub method: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub account: ::prost::alloc::string::String,
     #[prost(string, tag="4")]
-    pub wsteth_amount: ::prost::alloc::string::String,
+    pub amount: ::prost::alloc::string::String,
     #[prost(message, optional, tag="5")]
-    pub wsteth_transfer: ::core::option::Option<Transfer>,
+    pub transfer: ::core::option::Option<Transfer>,
     #[prost(string, tag="6")]
     pub tx_hash: ::prost::alloc::string::String,
     #[prost(string, tag="7")]
@@ -91,6 +67,7 @@ pub enum ActionType {
     Wrap = 0,
     Unwrap = 1,
     Send = 2,
+    Other = 3,
 }
 impl ActionType {
     /// String value of the enum field names used in the ProtoBuf definition.
@@ -102,6 +79,7 @@ impl ActionType {
             ActionType::Wrap => "wrap",
             ActionType::Unwrap => "unwrap",
             ActionType::Send => "send",
+            ActionType::Other => "other",
         }
     }
     /// Creates an enum from field names used in the ProtoBuf definition.
@@ -110,6 +88,7 @@ impl ActionType {
             "wrap" => Some(Self::Wrap),
             "unwrap" => Some(Self::Unwrap),
             "send" => Some(Self::Send),
+            "other" => Some(Self::Other),
             _ => None,
         }
     }
